b6179ef603a49c594d3e79b96850a6bd16d655b6
george <georgios.perivolarakis@gamelounge.com>
2020-04-10 19:53:41 +0200

    fix: reverted to mock data

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -151,7 +151,7 @@ def venues():
             "num_upcoming_shows": 0,
         }]
     }]
-    return render_template('pages/venues.html', areas=venues)
+    return render_template('pages/venues.html', areas=data)
 
 
 @app.route('/venues/search', methods=['POST'])
