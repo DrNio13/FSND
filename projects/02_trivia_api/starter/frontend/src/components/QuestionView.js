edd1b883f04fbab944f5ff0ba1e4cdad49fc5204
george <georgios.perivolarakis@gamelounge.com>
2020-04-20 08:20:00 +0200

    fix: play tab list rendering properly

--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -23,7 +23,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `/questions?page=${this.state.page}`,
       type: "GET",
       success: (result) => {
         this.setState({
@@ -62,7 +62,7 @@ class QuestionView extends Component {
 
   getByCategory = (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `/categories/${id}/questions`,
       type: "GET",
       success: (result) => {
         this.setState({
@@ -81,7 +81,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/search/questions`,
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
@@ -109,7 +109,7 @@ class QuestionView extends Component {
     if (action === 'DELETE') {
       if (window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
-          url: `/questions/${id}`, //TODO: update request URL
+          url: `/questions/${id}`,
           type: "DELETE",
           success: (result) => {
             this.getQuestions();
@@ -129,8 +129,8 @@ class QuestionView extends Component {
         <div className="categories-list">
           <h2 onClick={() => { this.getQuestions() }}>Categories</h2>
           <ul>
-            {Object.keys(this.state.categories).map((id, ) => (
-              <li key={id} onClick={() => { this.getByCategory(id) }}>
+            {Object.keys(this.state.categories).map((id) => (
+              this.state.categories[id] && <li key={id} onClick={() => { this.getByCategory(id) }}>
                 {this.state.categories[id]}
                 <img className="category" src={`${this.state.categories[id]}.svg`} />
               </li>
